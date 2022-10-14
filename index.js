a657f0ec3e0f6abc7962f6c93750fb18ea7c86cc
Joao Santos <joao.santosjoaos@DESKTOP-RG88VTM MINGW64 ~$ pwd/c/Users/joaosjoaos@DESKTOP-RG88VTM MINGW64 ~$ ls AppData/'Application Data'@ Contacts/ Cookies@'Definies Locais'@ Desktop/ Documents/ Downloads/ Favorites/ IntelGraphicsProfiles/ Links/'Menu Iniciar'@ Modelos@ Music/ NTUSER.DAT NTUSER.DAT{1caa2eaa-0672-11ed-96d9-9cb6d0c318c4}.TM.blf NTUSER.DAT{1caa2eaa-0672-11ed-96d9-9cb6d0c318c4}.TMContainer00000000000000000001.regtrans-ms NTUSER.DAT{1caa2eaa-0672-11ed-96d9-9cb6d0c318c4}.TMContainer00000000000000000002.regtrans-ms NetHood@ OneDrive/'Os Meus Documentos'@ Pictures/ PrintHood@ Recent@'Saved Games'/ Searches/ SendTo@ Videos/ ntuser.dat.LOG1 ntuser.dat.LOG2 ntuser.inijoaos@DESKTOP-RG88VTM MINGW64 ~$joaos@DESKTOP-RG88VTM MINGW64 ~$ cd Desktopjoaos@DESKTOP-RG88VTM MINGW64 ~/Desktop$ cd MyProjectsjoaos@DESKTOP-RG88VTM MINGW64 ~/Desktop/MyProjects$ cd clone https://github.com/jpbs18/GuessNumber.gitbash: cd: too many argumentsjoaos@DESKTOP-RG88VTM MINGW64 ~/Desktop/MyProjects$ git config --global user.name Joao>
2022-10-14 16:55:28 +0100

    First try

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
 const prompts = require("prompts");
-const {getFullArticles2, get40News2, getPoints2, getFirstArticle2} = require("./requests.js");
-
-
-
-async function example5(){
-
-	const response = await prompts({
-		type: "select",
-		name: "value",
-		message: "Wich API you want to consult?",
-		choices: [
-			{title: "Algolia", value:"algolia"},
-			{title: "DeepL", value: "deepL"}
-		],
-		hint: "Just use arrow to select and then press enter"
-	});
-
-	console.log(response);
-}
-
-
+const {getFullArticles2, get40News2, getPoints2, getFirstArticle2, getNumberOfNewsBuffered} = require("./requests.js");
 
 
 
@@ -27,63 +7,38 @@ async function example6(){
 
 	const question = await prompts(
 		{
-			type: "select",
+			type: "number",
 			name: "value",
-			message: "Wich API you want to consult?",
-			choices: [
-				{title: "Algolia", value:"algolia"},
-				{title: "DeepL", value: "deepL"}
-			],
-			hint: "Just use arrow to select and then press enter"
+			message: "How many articles do you want?",
 		});
 
+	const number = question.value;
+	
 
-	if(question.value === "algolia"){
+	const question2 = await prompts(
+		{
+			type: "select",
+			name: "value",
+			message: "Wich type of search do you wish??",
+			choices: [
+				{ title: "Buffered", value: "buffered" },
+				{ title: "Iterator", value: "iterator" },
+			],
+		});
 
-		const question2 = await prompts(
-			{
-				type: "select",
-				name: "request",
-				message: "What request you want to make to this API?",
-				choices:[
-					{title: "Fetch the first news details", value: 1},
-					{title: "Fetch the points from a news page", value: 2},
-					{title: "Fetch 40 news from 2 pages", value: 3},
-					{title: "Fetch the details from a full news page", value: 4}
-				],
-				hint: "Just use arrow to select and then press enter"
-			});
+	const type = question2.value;	
+	
+	
+	if(type === "buffered"){
 
-		switch(question2.request){
-		case 1 : return getFirstArticle2();
-		case 2 : return getPoints2();
-		case 3 : return get40News2();
-		case 4: return getFullArticles2();
-		}
+		return getNumberOfNewsBuffered(number);
+	}
+	else{
 
-	}else{
+		return getNumberOfNewsBuffered(number);
+	}
 
-		const question3 = await prompts(
-			{
-				type: "select",
-				name: "request",
-				message: "What request you want to make to this API?",
-				choices:[
-					{title: "Fetch the first 10 cars", value: 1},
-					{title: "Fetch the points from 10 cars", value: 2},
-					{title: "Fetch 40 cars form 2 pages", value: 3},
-					{title: "Fetch the details from a full cars page", value: 4}
-				],
-				hint: "Just use arrow to select and then press enter"
-			});
 
-		switch(question3.request){
-		case 1 : return "Not available yet";
-		case 2 : return "Not available yet";
-		case 3 : return "Not available yet";
-		case 4: return "Not available yet";
-		}
-	}	
 }
 
 example6().then(console.log);
--- requests.js
+++ requests.js
@@ -1,52 +1,44 @@
 
-async function getFirstArticle2(){
 
-	const response = await fetch("http://hn.algolia.com/api/v1/search");
-	const json = await response.json();
-
-	const firstNewsId = json.hits[0].objectID;
-
-	const response2 = await fetch(`http://hn.algolia.com/api/v1/items/${firstNewsId}`);
-	const json2 = await response2.json();
-	return json2;
-}
-
-
-
-async function getPoints2(){
-
-	const response = await fetch("http://hn.algolia.com/api/v1/search?page=0");
-	const json = await response.json();
-
-	const arrayPoints = json.hits.map(e => e.points);
-	return arrayPoints.reduce((acc, curr) => acc + curr, 0);
-}
-
-
-
-async function get40News2(){
-
-	return Promise.all([
-		fetch("http://hn.algolia.com/api/v1/search?page=0").then(response => response.json()).then(json => json.hits),
-		fetch("http://hn.algolia.com/api/v1/search?page=1").then(response => response.json()).then(json => json.hits)
-	]).then(arr => arr.flat());
+function getNumberOfNewsBuffered(numberOfArticles){
 	
-}
+	/*const response = function () {
+		let finalArr = [];
+		let number = 0;
+		let numberOfArticlesToRequest = numberOfArticles;
+		async function array(numberOfPage){ await fetch(`http://hn.algolia.com/api/v1/search?page=${numberOfPage}`)
+		.then(response => response.json())
+		.then(json => json.hits)
+		//.then(hits => hits.filter(e => e != undefined))
+		.then(hits => {
+			
+			let numberOfRequests = 0;
+			if(numberOfArticlesToRequest > 20){
+				numberOfRequests = 20;
+				numberOfArticlesToRequest -= numberOfRequests;
+			} 
+
+			for(let i = 0; i < 20; i++) {
+				const article = {
+					title: hits[i].title,
+					url: hits[i].url
+				}
+				finalArr.push(article);
+			}
+		
+			if(finalArr.length<numberOfArticles){
+				array(++number);
+			}
+			
+		})};
+		array(number);
+		return finalArr;
+	}
+	
+}*/
 
 
-async function getFullArticles2(){
-
-	const response = await fetch("http://hn.algolia.com/api/v1/search");
-	const json = await response.json();
-
-	const idArray = json.hits.map(e => e.objectID);
-
-	const responseArray = idArray.map(e => fetch("http://hn.algolia.com/api/v1/items/" + e));
-	const jsonArray = responseArray.map(e => e.then(e => e.json()));
-
-	return Promise.all(jsonArray);	
-}
 
 
 
-module.exports = {getFullArticles2, get40News2, getPoints2, getFirstArticle2};
+module.exports = {getNumberOfNewsBuffered};
